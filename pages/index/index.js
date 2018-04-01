d3c012bc757f7902de01c1ce7a5b11db6795108b
Patrick <403983161@qq.con>
2018-04-01 16:30:07 +0800

    index add

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,7 +11,26 @@ Page({
         }
     },
     data: {
-
+        showPresent: false,
+        bannerData:[{url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png'}],
+        scrollShopData:[
+            {url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png'},
+            {url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png'},
+            {url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png'},
+            {url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png'}
+        ],
+        brandData:[
+            {url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png',name:'零食'},
+            {url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png',name:'零食'},
+            {url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png',name:'零食'},
+            {url:'https://www.weiyoutong.cn/weiyoutong/Public/Home/images/nail/close_btn.png',name:'零食'}
+        ],
+    },
+    toggleKey: function (event) {
+        var key = event.currentTarget.dataset.key;
+        this.setData({
+            [key] : !this.data[key]
+        })
     },
     onLoad: function (options) {
 
