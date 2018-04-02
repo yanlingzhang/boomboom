4029d61b278a828d960e1ed7a15d0bbda1d01e3d
FuYiwei <835718378@qq.com>
2018-04-02 20:39:46 +0800

    url 跳转流程处理

--- pages/addAddress/addAddress.js
+++ pages/addAddress/addAddress.js
@@ -34,6 +34,12 @@ Page(extend({}, Field, Switch, {
       });
     }
   },
+  switch1Change: function (e){
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  },
+  switch2Change: function (e){
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  },
   /* piker-view 示例相关函数 */
   handleDateFieldClick() {
     this.setData({
